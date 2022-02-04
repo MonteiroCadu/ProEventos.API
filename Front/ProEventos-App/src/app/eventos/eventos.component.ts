a961dd65f7c97a9422ab9f5526d7ecffe5129846
MonteiroCadu <cadumonteiro.setmidia@gmail.com>
2022-02-04 12:27:27 -0300

    Alterção na grib de busca: melhoria no layout

--- Front/ProEventos-App/src/app/eventos/eventos.component.ts
+++ Front/ProEventos-App/src/app/eventos/eventos.component.ts
@@ -9,20 +9,29 @@ import { Component, OnInit } from '@angular/core';
 export class EventosComponent implements OnInit {
 
   public eventos: any = [];
+  public eventosFiltrados: any = [];
+
   public exibirImagem: boolean = true;
   private _filtroLista: string = '';
   constructor(private http: HttpClient) { }
 
-  public get filtroLista(){
+  public get filtroLista() : string {
     return this._filtroLista;
   }
-  public set filtroLista(filtro:string ){
-    this._filtroLista = filtro;
-  }
-
-  public filtrar(){
+  public set filtroLista(value: string ) {
+    this._filtroLista = value;
+    this.eventosFiltrados = this._filtroLista ? this.filtrarEventos(this._filtroLista) : this.eventos;
 
   }
+  filtrarEventos(filtrarPor: string) : any {
+
+    filtrarPor = filtrarPor.toLocaleLowerCase();
+
+    return this.eventos.filter(
+       (evento: {tema:string}) => evento.tema.toLocaleLowerCase().indexOf(filtrarPor) !== -1
+    )
+  }
+
 
   setExibirImagem(){
     this.exibirImagem = !this.exibirImagem
@@ -35,9 +44,12 @@ export class EventosComponent implements OnInit {
   public getEventos(): void{
     this.http.get('https://localhost:5001/api/Eventos').subscribe(
       {
-        next: (v) => this.eventos = v,
+        next: (v) => {
+              this.eventos = v,
+              this.eventosFiltrados = v
+            },
         error: (e) => console.error(e),
-        complete: () => console.info('complete')
+        complete: () =>  console.info('complete')
       }
 
     );
