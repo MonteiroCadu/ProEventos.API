44bd933e1a77bd758ca04178ba90cf0466b1daa5
MonteiroCadu <cadumonteiro.setmidia@gmail.com>
2022-02-07 12:28:46 -0300

    correcao de nome da propriedade Lotes

--- Back/src/ProEventos.Domain/Evento.cs
+++ Back/src/ProEventos.Domain/Evento.cs
@@ -8,13 +8,13 @@ namespace ProEventos.Domain
         public int Id {get;set;}
         public string Tema {get;set;} 
         public string Local {get;set;}
-        public string lote {get;set;}
+        
         public int QtdPessoas {get;set;}
         public string DataEvento {get;set;}
         public string ImageUrl {get;set;}
         public string Telefone { get; set; }
         public string Email { get; set; }
-        public IEnumerable<Lote> Lote { get; set; }
+        public IEnumerable<Lote> Lotes { get; set; }
         public IEnumerable<RedeSocial> RedesSociais { get; set; }
 
 
